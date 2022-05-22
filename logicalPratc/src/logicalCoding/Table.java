9b5fe3792e4553f9c3785bbb911c0956fc45b6fc
Tusharnm <tushar.05masne@gmail.com>
2022-05-22 13:21:05 +0530

    logical code first commit

---
+++ logicalPratc/src/logicalCoding/Table.java
@@ -0,0 +1,17 @@
+package logicalCoding;
+
+public class Table {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x=19;
+		for(int i=1;i<=10;i++) {
+			
+			System.out.println(i*x);
+			
+		}
+		
+	}
+
+}
